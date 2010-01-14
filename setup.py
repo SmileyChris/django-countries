295f7172e356e979bea6e26bce09214271f59ebb
Chris Beaven <smileychris@gmail.com>
2010-01-14 22:33:09 +1300

    Update setup.py to reflect the change to django-countries (since the "other" one seems to have stagnated anyway).

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name='django-countries-2',
-      version='1.0',
+setup(name='django-countries',
+      version='1.0a1',
+      description="Python Port of John Gruber's titlecase.pl",
       author='Chris Beaven',
       author_email='smileychris@gmail.com',
       packages=find_packages(),
       package_data={'django_countries': ['bin/*.py', 'media/*.*']},
       # titlecase PYPI is broken, copied the module directly for now (in /bin)
-#      requires=['titlecase']
+#      requires=['titlecase'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Framework :: Django',
+      ],
 )
-
