a6ad4320bf5bef375cca5905458ab2e4338a0a9e
Chris Beaven <smileychris@gmail.com>
2012-10-10 10:39:07 +1300

    Bump to 1.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def long_description():
 
 setup(
     name='django-countries',
-    version='1.3',
+    version='1.4',
     description='Provides a country field for Django models.',
     long_description=long_description(),
     author='Chris Beaven',
