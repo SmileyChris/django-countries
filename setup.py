5b3a21390f5edf501aa47db921422d3198719099
Chris Beaven <smileychris@gmail.com>
2013-12-19 15:50:56 +1300

    Mark version as 2.0 beta for now

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def long_description():
 
 setup(
     name='django-countries',
-    version='2.0',
+    version='2.0b',
     description='Provides a country field for Django models.',
     long_description=long_description(),
     author='Chris Beaven',
