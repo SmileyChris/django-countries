d6d9a5283206027dd0a0a767a2fb0a864dbf47c0
Chris Beaven <smileychris@gmail.com>
2014-08-05 08:53:53 +1000

    Tidy up ioc_data.py checks

--- django_countries/ioc_data.py
+++ django_countries/ioc_data.py
@@ -215,6 +215,6 @@ def check_ioc_countries():
     from django_countries.data import COUNTRIES
 
     print("Checking if all IOC codes map correctly")
-    for key in ISO_TO_IOC.keys():
-        assert COUNTRIES.get(key, '') != '', 'No ISO code for %s' % key
+    for key in ISO_TO_IOC:
+        assert COUNTRIES.get(key), 'No ISO code for %s' % key
     print("Finished checking IOC codes")
