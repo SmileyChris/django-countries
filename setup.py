c1798354cb25f4e748abbf8d4c1b420cdb73519f
Chris Beaven <smileychris@gmail.com>
2014-09-03 11:18:17 +1200

    Bump tests to use Django 1.7
    Refs #69

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Framework :: Django',
     ],
 )
