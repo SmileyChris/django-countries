0cb5d61fb1e3f0954ef77ab5635735022fef14c8
Chris Beaven <smileychris@gmail.com>
2015-02-24 15:14:00 +1300

    Prepare for version 3.2 release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read_files(*filenames):
 
 setup(
     name='django-countries',
-    version='3.1.1',
+    version='3.2',
     description='Provides a country field for Django models.',
     long_description=read_files('README.rst', 'CHANGES.rst'),
     author='Chris Beaven',
