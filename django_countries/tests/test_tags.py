dfa9f359c2b2cfba1124a146ff759fbad728e86d
Chris Beaven <smileychris@gmail.com>
2015-07-22 08:57:11 +1200

    Add another tags test

--- django_countries/tests/test_tags.py
+++ django_countries/tests/test_tags.py
@@ -5,14 +5,21 @@ from django.utils import translation
 
 class TestCountriesTags(TestCase):
 
-    TEMPLATE_COUNTRY = Template("{% load countries %}{% get_country code as country %}{{ country }}")
-    TEMPLATE_NAME = Template("{% load countries %}{% get_country code as country %}{{ country.name }}")
+    TEMPLATE_COUNTRY = Template(
+        "{% load countries %}{% get_country code as country %}{{ country }}")
+    TEMPLATE_NAME = Template(
+        "{% load countries %}{% get_country code as country %}"
+        "{{ country.name }}")
 
     def test_country(self):
         rendered = self.TEMPLATE_COUNTRY.render(Context({'code': 'BR'}))
         self.assertEqual(rendered, 'BR')
 
     def test_country_name(self):
+        rendered = self.TEMPLATE_NAME.render(Context({'code': 'BR'}))
+        self.assertEqual(rendered, 'Brazil')
+
+    def test_country_name_translated(self):
         with translation.override('pt-BR'):
             rendered = self.TEMPLATE_NAME.render(Context({'code': 'BR'}))
         self.assertEqual(rendered, 'Brasil')
