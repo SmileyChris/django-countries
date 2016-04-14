a8000bf1b55ca6a72a348bc311f399659bfb88f5
Simon Charette <charette.s@gmail.com>
2016-04-14 15:47:30 -0400

    Stopped using assignment_tag in Django 1.9.
    It has been deprecated in favor of simple_tag.

--- django_countries/templatetags/countries.py
+++ django_countries/templatetags/countries.py
@@ -1,10 +1,11 @@
-from django import template
+from django import VERSION, template
 from django_countries.fields import Country
 
 
 register = template.Library()
 
+simple_tag = register.assignment_tag if VERSION < (1, 9) else register.simple_tag
 
-@register.assignment_tag
+@simple_tag
 def get_country(code):
     return Country(code=code)
