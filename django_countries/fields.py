b1aa9f4b1dbbfdd64f1c764b128993ec2fa3915d
Chris Beaven <smileychris@gmail.com>
2016-08-15 23:51:51 +1000

    Some code tidy-up

--- django_countries/fields.py
+++ django_countries/fields.py
@@ -126,21 +126,19 @@ class Country(object):
 
         # Don't really like magic numbers, but this is the code point for [A]
         # (Regional Indicator A), minus the code point for ASCII A. By adding
-        # this to the uppercase characters making up the ISO 3166-1 alpha-2 codes
-        # we can get the flag.
-
+        # this to the uppercase characters making up the ISO 3166-1 alpha-2
+        # codes we can get the flag.
         OFFSET = 127397
-        points = map(lambda x: ord(x) + OFFSET, self.code.upper())
+        points = [ord(x) + OFFSET for x in self.code.upper()]
 
         try:
             # Python 3 is simple: we can just chr() the unicode points.
             return chr(points[0]) + chr(points[1])
         except ValueError:
-            # Python 2 requires us to be a bit more creative. We could use unichr(),
-            # but that only works if the python has been compiled with wide unicode
-            # support. This method should always work.
+            # Python 2 requires us to be a bit more creative. We could use
+            # unichr(), but that only works if the python has been compiled
+            # with wide unicode support. This method should always work.
             return ('\\U%08x\\U%08x' % tuple(points)).decode('unicode-escape')
-
 
     @staticmethod
     def country_from_ioc(ioc_code, flag_url=''):
--- django_countries/tests/settings.py
+++ django_countries/tests/settings.py
@@ -15,3 +15,21 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
 )
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
