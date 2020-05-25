b6ba6f2f75f608312994332901e826be9a0cc781
Joshua Chinemezu <joshuachinemezu@gmail.com>
2020-05-25 12:15:50 +0400

    Add multiple args in country field serializer

--- django_countries/serializer_fields.py
+++ django_countries/serializer_fields.py
@@ -7,17 +7,35 @@ from django_countries import countries
 class CountryField(serializers.ChoiceField):
     def __init__(self, *args, **kwargs):
         self.country_dict = kwargs.pop("country_dict", None)
+        self.multiple = kwargs.pop("multiple", False)
         field_countries = kwargs.pop("countries", None)
         self.countries = field_countries if field_countries else countries
         super().__init__(self.countries, *args, **kwargs)
 
     def to_representation(self, obj):
-        code = self.countries.alpha2(obj)
-        if not code:
-            return ""
-        if not self.country_dict:
-            return code
-        return {"code": code, "name": force_str(self.countries.name(obj))}
+        if self.multiple:
+            country_list = []
+            for key, code in enumerate(obj):
+                country_list.append(
+                    {
+                        'code': self.countries.alpha2(obj[key]),
+                        'name': self.countries.name(obj[key]),
+                        'alpha3': self.countries.alpha3(obj[key]),
+                    }
+                )
+        else:
+            code = self.countries.alpha2(obj)
+            if not code:
+                return ""
+            if not self.country_dict:
+                return code
+            country_list = {
+                "code": code,
+                "name": force_str(self.countries.name(obj)),
+                "alpha3": force_str(self.countries.alpha3(obj))
+            }
+
+        return country_list
 
     def to_internal_value(self, data):
         if not self.allow_blank and data == "":
