5b76f193250ce9eb19b5a824eb2059e616e6f536
Chris Beaven <smileychris@gmail.com>
2021-05-11 22:49:41 +1200

    Better method for language mapping. Fixes #347

--- django_countries/release.py
+++ django_countries/release.py
@@ -27,7 +27,6 @@ def translations(data):
         cmd_pull(argv=["-a", "--minimum-perc=60"], path_to_tx=find_dot_tx())
     finally:
         logger.handlers = _handlers
-    fix_locale_paths()
     _cwd = os.getcwd()
     os.chdir(os.path.dirname(django_countries.__file__))
     try:
@@ -35,16 +34,6 @@ def translations(data):
         execute_command(["git", "add", "locale"])
     finally:
         os.chdir(_cwd)
-
-
-def fix_locale_paths():
-    lpath = os.path.join(os.path.dirname(django_countries.__file__), "locale")
-    for name in os.listdir(lpath):
-        if re.match(r"\w\w-\w{3}", name):
-            new_path = os.path.join(lpath, name.replace("-", "_", 1))
-            if os.path.exists(new_path):
-                shutil.rmtree(new_path)
-            os.rename(os.path.join(lpath, name), new_path)
 
 
 if __name__ == "__main__":
