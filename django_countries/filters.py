SmileyChris/django-countries

commit 04662d1e282d7d3ffb8ff7701885149d27f633bd

Support Django 5.0
co-authored-by: Julian Tenschert <14051100+JulianTenschert@users.noreply.github.com>

django_countries/filters.py
@@ -1,3 +1,4 @@
+import django
 from django.contrib import admin
 from django.utils.encoding import force_str
 from django.utils.translation import gettext_lazy as _
@@ -22,8 +23,12 @@ class CountryFilter(admin.FieldListFilter):
             "display": _("All"),
         }
         for lookup, title in self.lookup_choices(changelist):
+            if django.VERSION >= (5, 0):
+                selected = force_str(lookup) in value
+            else:
+                selected = force_str(lookup) == value
             yield {
-                "selected": value == force_str(lookup),
+                "selected": selected,
                 "query_string": changelist.get_query_string(
                     {self.field.name: lookup}, []
                 ),

django_countries/fields.py
@@ -1,7 +1,9 @@
 import re
+import sys
 from typing import Any, Iterable, Optional, Tuple, Type, Union, cast
 from urllib import parse as urlparse
 
+import django
 from django import forms
 from django.contrib.admin.filters import FieldListFilter
 from django.core import checks, exceptions
@@ -18,9 +20,14 @@ _entry_points: Iterable[Any]
 try:
     import importlib.metadata
 
-    _entry_points = importlib.metadata.entry_points().get(
-        "django_countries.Country", []
-    )
+    if sys.version_info >= (3, 10):
+        _entry_points = importlib.metadata.entry_points(
+            group="django_countries.Country"
+        )
+    else:
+        _entry_points = importlib.metadata.entry_points().get(
+            "django_countries.Country", []
+        )
 except ImportError:  # Python <3.8
     import pkg_resources
 
@@ -229,12 +236,14 @@ class CountryDescriptor:
 
 
 class LazyChoicesMixin(widgets.LazyChoicesMixin):
-    def _set_choices(self, value):
-        """
-        Also update the widget's choices.
-        """
-        super()._set_choices(value)
-        self.widget.choices = value
+    if django.VERSION < (5, 0):
+
+        def _set_choices(self, value):
+            """
+            Also update the widget's choices.
+            """
+            super()._set_choices(value)
+            self.widget.choices = value
 
 
 _Choice = Tuple[Any, str]
@@ -278,7 +287,11 @@ class CountryField(CharField):
         self.multiple = kwargs.pop("multiple", None)
         self.multiple_unique = kwargs.pop("multiple_unique", True)
         self.multiple_sort = kwargs.pop("multiple_sort", True)
-        kwargs["choices"] = self.countries
+        if django.VERSION >= (5, 0):
+            # Use new lazy callable support
+            kwargs["choices"] = lambda: self.countries
+        else:
+            kwargs["choices"] = self.countries
         if "max_length" not in kwargs:
             # Allow explicit max_length so migrations can correctly identify
             # changes in the multiple CountryField fields when new countries are
@@ -396,19 +409,45 @@ class CountryField(CharField):
             kwargs["countries"] = self.countries.__class__
         return name, path, args, kwargs
 
-    def get_choices(self, include_blank=True, blank_choice=None, *args, **kwargs):
-        if blank_choice is None:
+    if django.VERSION >= (5, 0):
+
+        def get_choices(
+            self,
+            include_blank=True,
+            blank_choice=BLANK_CHOICE_DASH,
+            limit_choices_to=None,
+            ordering=(),
+        ):
+            if self.multiple:
+                include_blank = False
             if self.blank_label is None:
                 blank_choice = BLANK_CHOICE_DASH
             else:
                 blank_choice = [("", self.blank_label)]
-        if self.multiple:
-            include_blank = False
-        return super().get_choices(
-            include_blank=include_blank, blank_choice=blank_choice, *args, **kwargs
-        )
+            return super().get_choices(
+                include_blank=include_blank,
+                blank_choice=blank_choice,
+                limit_choices_to=limit_choices_to,
+                ordering=ordering,
+            )
 
-    get_choices = lazy(get_choices, list)
+    else:
+
+        def get_choices(  # type: ignore [misc]
+            self, include_blank=True, blank_choice=None, *args, **kwargs
+        ):
+            if blank_choice is None:
+                if self.blank_label is None:
+                    blank_choice = BLANK_CHOICE_DASH
+                else:
+                    blank_choice = [("", self.blank_label)]
+            if self.multiple:
+                include_blank = False
+            return super().get_choices(
+                include_blank=include_blank, blank_choice=blank_choice, *args, **kwargs
+            )
+
+        get_choices = lazy(get_choices, list)
 
     def formfield(self, **kwargs):
         kwargs.setdefault(

django_countries/widgets.py
@@ -2,6 +2,7 @@ import copy
 from typing import List, Union
 from urllib import parse as urlparse
 
+import django
 from django.forms import widgets
 from django.utils.functional import Promise
 from django.utils.html import escape
@@ -20,31 +21,35 @@ ChoiceList = List[List[Union[int, str]]]
 
 
 class LazyChoicesMixin:
-    def get_choices(self) -> ChoiceList:
-        """
-        When it's time to get the choices, if it was a lazy then figure it out
-        now and memoize the result.
-        """
-        if isinstance(self._choices, Promise):
-            self._choices: ChoiceList = list(self._choices)
-        return self._choices
+    if django.VERSION < (5, 0):
 
-    def set_choices(self, value: ChoiceList):
-        self._set_choices(value)
+        def get_choices(self) -> ChoiceList:
+            """
+            When it's time to get the choices, if it was a lazy then figure it out
+            now and memoize the result.
+            """
+            if isinstance(self._choices, Promise):
+                self._choices: ChoiceList = list(self._choices)
+            return self._choices
 
-    choices = property(get_choices, set_choices)
+        def set_choices(self, value: ChoiceList):
+            self._set_choices(value)
 
-    def _set_choices(self, value: ChoiceList):
-        self._choices = value
+        choices = property(get_choices, set_choices)
+
+        def _set_choices(self, value: ChoiceList):
+            self._choices = value
 
 
 class LazySelectMixin(LazyChoicesMixin):
-    def __deepcopy__(self, memo):
-        obj = copy.copy(self)
-        obj.attrs = self.attrs.copy()
-        obj.choices = copy.copy(self._choices)
-        memo[id(self)] = obj
-        return obj
+    if django.VERSION < (5, 0):
+
+        def __deepcopy__(self, memo):
+            obj = copy.copy(self)
+            obj.attrs = self.attrs.copy()
+            obj.choices = copy.copy(self._choices)
+            memo[id(self)] = obj
+            return obj
 
 
 class LazySelect(LazySelectMixin, widgets.Select):  # type: ignore

setup.cfg
@@ -26,6 +26,7 @@ classifiers =
     Framework :: Django :: 4.0
     Framework :: Django :: 4.1
     Framework :: Django :: 4.2
+    Framework :: Django :: 5.0
 project_urls =
     Change Log = https://github.com/SmileyChris/django-countries/blob/main/CHANGES.rst
     Source Code = https://github.com/SmileyChris/django-countries

tox.ini
@@ -36,10 +36,10 @@ deps =
     pyuca,legacy: pyuca
     legacy: Django==3.2.*
     previous: Django==4.0.*
-    latest: Django==4.2.*
+    latest: Django~=5.0b1
     latest: graphene-django==3.0.*
 depends = coverage_setup
-commands = coverage run -m pytest
+commands = coverage run -m pytest {posargs}
 
 [testenv:readme]
 skip_install = True

commit bae099f8008c47519a1653b620d44155bfe532bb

Fix TypeError in CountryFilter.choices()
When no country is selected, self.used_parameters.get(self.field.name)
will return None. This raises a TypeError when CountryFilter.choices()
tries to check whether a specific country choice is currently selected.

Avoid this by checking that the parameter is not empty similarly to how
Django's built-in admin.filters.RelatedFieldListFilter checks for this.

Fixes: 04662d1e282d ("Support Django 5.0")

django_countries/filters.py
@@ -24,7 +24,7 @@ class CountryFilter(admin.FieldListFilter):
         }
         for lookup, title in self.lookup_choices(changelist):
             if django.VERSION >= (5, 0):
-                selected = force_str(lookup) in value
+                selected = value is not None and force_str(lookup) in value
             else:
                 selected = force_str(lookup) == value
             yield {